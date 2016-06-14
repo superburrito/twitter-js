d9a71bab81547431d8f82657318ae6181892f73d
superburrito <chuayaohui@gmail.com>
2016-06-14 14:58:14 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = new express();
-
+var chalk = require('chalk');
 app.listen(3000);
+
+
+app.use(function(req,res,next){
+  console.log(chalk.yellow(req.method) + ' ' + chalk.blue(req.path) + ' ');
+  next(); 
+});
+
+app.use('/special/', function(req,res,next){
+  res.send("You've reached the special area...");
+  next(); 
+});
 
 app.get('/', function(req,res,next){
   res.send('Welcome!');
 });
+
+app.get('/news', function(req,res,next){
+  res.send('This is the news section!');
+});
+
 
