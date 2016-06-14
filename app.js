7ee6f9f5e5c44ab32b20110a8562c06de33e3bf9
superburrito <chuayaohui@gmail.com>
2016-06-14 15:47:26 -0400

    rendered once

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = new express();
 var chalk = require('chalk');
-app.listen(3000);
+var swig = require('swig');
+swig.setDefaults({ cache: false });
 
+var whatever = {
+  bar: 'Title',
+  foo: [
+    { name: 'John'},
+    { name: 'Yao'},
+    { name: 'Reed'}
+  ]
+};
+
+
+app.engine('html', swig.renderFile);
+app.set('view engine','html');
+app.set('views',__dirname + '/views');
 
 app.use(function(req,res,next){
+  res.render('somethingelse', whatever);
   console.log(chalk.yellow(req.method) + ' ' + chalk.blue(req.path) + ' ');
-  next(); 
 });
+
+
+app.listen(3000);
 
 app.use('/special/', function(req,res,next){
   res.send("You've reached the special area...");
